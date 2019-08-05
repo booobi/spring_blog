682730b322bceee966c10486a7dfafea21d0a052
booobi <qqwweee59@gmail.com>
2019-08-05 15:02:00 +0300

    Add ArticleBindingModel (create Article functionality)

--- src/main/java/bobi/blog/Controller/ArticleController.java
+++ src/main/java/bobi/blog/Controller/ArticleController.java
@@ -1,12 +1,18 @@
 package bobi.blog.Controller;
 
+import bobi.blog.bindingModel.ArticleBindingModel;
+import bobi.blog.entities.Article;
+import bobi.blog.entities.User;
 import bobi.blog.repository.ArticleRepository;
 import bobi.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -21,5 +27,18 @@ public class ArticleController {
         model.addAttribute("view", "article/create");
 
         return "base-layout";
+    }
+
+    @PostMapping("article/create")
+    @PreAuthorize("isAuthenticated()")
+    public String createProcess(ArticleBindingModel articleBindingModel){
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = this.userRepository.findByEmail(principal.getUsername());
+
+        Article article = new Article(articleBindingModel.getTitle(), articleBindingModel.getContent(), user);
+
+        this.articleRepository.saveAndFlush(article);
+
+        return "redirect:/";
     }
 }
