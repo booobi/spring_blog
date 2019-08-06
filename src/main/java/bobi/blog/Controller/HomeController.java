cac34cb376faf70576218d387d40fde544df1758
booobi <qqwweee59@gmail.com>
2019-08-06 11:58:41 +0300

    change home functionality to be category oriented, add list categories functionality

--- src/main/java/bobi/blog/Controller/HomeController.java
+++ src/main/java/bobi/blog/Controller/HomeController.java
@@ -1,25 +1,44 @@
 package bobi.blog.Controller;
 
 import bobi.blog.entities.Article;
-import bobi.blog.repository.ArticleRepository;
+import bobi.blog.entities.Category;
+import bobi.blog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class HomeController {
     @Autowired
-    private ArticleRepository articleRepository;
+    private CategoryRepository categoryRepository;
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Article> articles = articleRepository.findAll();
+        List<Category> categories = this.categoryRepository.findAll();
+
+        model.addAttribute("categories", categories);
+        model.addAttribute("view", "home/index");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/category/{id}")
+    public String listArticles(@PathVariable Integer id, Model model){
+        if(!this.categoryRepository.exists(id)) {
+            return "redirect:/";
+        }
+
+        Category category = this.categoryRepository.findOne(id);
+        Set<Article> articles = category.getArticles();
 
         model.addAttribute("articles", articles);
-        model.addAttribute("view", "home/index");
+        model.addAttribute("category", category);
+        model.addAttribute("view", "home/list-articles");
 
         return "base-layout";
     }
