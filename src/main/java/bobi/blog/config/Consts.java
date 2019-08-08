4d67e337c4e7462bf7efd0999bafcdda6e5cb77f
booobi <qqwweee59@gmail.com>
2019-08-08 14:45:45 +0300

    remove forward slash from REST_SERVICES_URL

--- src/main/java/bobi/blog/config/Consts.java
+++ src/main/java/bobi/blog/config/Consts.java
@@ -2,5 +2,5 @@ package bobi.blog.config;
 
 public final class Consts {
     public static final String BASE_LAYOUT = "base-layout";
-    public static final String REST_SERVICES_URL = "/rest/api/";
+    public static final String REST_SERVICES_URL = "/rest/api";
 }
